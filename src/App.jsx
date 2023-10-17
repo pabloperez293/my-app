02bc5a956ccab80a1e0579c718e450dbdbd2111d
pablo <paperezpablo@gmail.com>
2023-10-17 00:08:55 -0300

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,113 @@
-import { TextField } from "@mui/material";
+import { TextField, Slide } from "@mui/material";
+import { CircularProgress } from "@mui/material";
+
 // Importaciones de fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faSun, faCloudSun } from "@fortawesome/free-solid-svg-icons";
 // linkeado el componente
 import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-
   const apiKey = process.env.REACT_APP_API_KEY;
 
   // console.log(process.env.REACT_APP_API_KEY)
 
   const [cityID, setCityName] = useState("3433955");
+  const [inputText, setInputText] = useState("");
+  const [data, setData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${apiKey}`
-    //   `https://api.openweathermap.org/data/2.5/weather?q=?${cityName}&appid=11f370f6b49545f3442d3ea3858319f9&units=metric`
-    // 
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${apiKey}`
     )
-      // fetch(` https://api.openweathermap.org/data/2.5/weather?id=${process.env.REACT_APP_API_ID}&appid=${process.env.REACT_APP_API_KEY}`)
-
       .then((res) => {
         if (res.status === 200) {
+          error && setError(false);
           return res.json();
         } else {
           throw new Error("Esta colocando un caracter/pais invalido");
         }
       })
       .then((data) => {
-        console.log(data);
-      });
-  }, []);
+        setData(data);
+        // console.log(data);
+      })
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, [cityID, error]);
+
+  // console.log(inputText)
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      setCityName(e.target.value);
+      setInputText("");
+    }
+  };
 
   return (
     <div className='container'>
       <div className='bg_img'>
-        <TextField variant='filled' label='Bucar..' className='input' />
-        <h1 className='city'> Buenos Aires</h1>
-        <div className='group'>
-          {/* Llamo al componente */}
-          <FontAwesomeIcon
-            icon={faSun}
-            style={{ color: "#e1ac19", height: "60px" }}
-          />
+        {!loading ? (
+          <>
+            <TextField
+              variant='filled'
+              label='Bucar..'
+              className='input'
+              error={error}
+              value={inputText}
+              onChange={(evt) => setInputText(evt.target.value)}
+              onKeyDown={handleSearch}
+              
+            />
 
-          <img src='' alt='' />
-          <h1>Clear</h1>
-        </div>
+            {/* a11y Accecibilidad  */}
+            {/* <TextField variant='filled' label='Bucar..' className='input' /> */}
 
-        <h1 className='temp'>5 °C</h1>
+            <h1 className='city'> {data.name}</h1>
+            <div className='group'>
+              {/* Llamo al componente */}
 
-        {/* <Slide direction="right" timeout={800} > */}
-        <div className='box_container'>
-          <div className='box'>
-            <p>Humedad</p>
-            <h1>12%</h1>
-          </div>
+              <FontAwesomeIcon
+                icon={faCloudSun}
+                style={{
+                  "--fa-primary-color": "#67828e",
+                  height: "60px",
+                  "--fa-secondary-color": "#ffdd00",
+                }}
+              />
 
-          <div className='box'>
-            <p>Vientos</p>
-            <h1>5 km/h</h1>
-          </div>
+              <img src={''} alt='' />
+              <h1>Clear</h1>
+            </div>
 
-          <div className='box'>
-            <p>Sensacion termica</p>
-            <h1> 4 °C</h1>
-          </div>
+            <h1 className='temp'>5 °C</h1>
 
-          {/* </Slide> */}
-        </div>
+            <Slide direction='right' timeout={800} in={!loading}>
+              <div className='box_container'>
+                <div className='box'>
+                  <p>Humedad</p>
+                  <h1>12%</h1>
+                </div>
+
+                <div className='box'>
+                  <p>Vientos</p>
+                  <h1>5 km/h</h1>
+                </div>
+
+                <div className='box'>
+                  <p>Sensacion termica</p>
+                  <h1> 4 °C</h1>
+                </div>
+              </div>
+            </Slide>
+          </>
+        ) : (
+          <CircularProgress />
+        )}
       </div>
     </div>
   );
